3a60b6aea2948d8a8201b1d45e3fee1c1bdd902f
jackmcguinness-1 <jack1mcguinness@gmail.com>
2022-09-18 20:51:40 +0100

    added some custom test handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
+mod serial;
+
+
 use vga_buffer::{
     Writer,
     ColourCode,
@@ -11,23 +17,76 @@ use vga_buffer::{
 
 use core::{panic::PanicInfo, fmt::Write};
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic (info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> !{
+    serial_println!("{}", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
 
-static HELLO: &[u8] = b"Hello World";
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]){
+    serial_println!("Running {} tests", tests.len());
+    for test in tests{
+        test.run();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub trait Testable{
+    fn run(&self);
+}
+
+impl<T> Testable for T
+where T: Fn()
+{
+    fn run(&self){
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]")
+    }
+}
+
+
+#[test_case]
+fn test(){
+    assert_eq!(1, 1);
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+
+    #[cfg(test)]
+    test_main();
 
     println!("First line");
     println!("Second line");
     println!("Hello world from new println macro!!");
 
-    panic!("we can now do panic messages, pretty nice!");
-
     loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode{
+    Success = 0x10,
+    Failed = 0x11
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode){
+    use x86_64::instructions::port::Port;
+
+    unsafe{
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32)
+    }
 }
